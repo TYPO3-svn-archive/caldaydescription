d06f573a63bfd1bd64e6c466df3d5d781afbf49d
thomaskowtsch <thomaskowtsch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-25 20:29:28 +0000

    inital working version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caldaydescription/trunk@53396 735d13b6-9817-0410-8766-e36946ffe9aa

--- hooks/class.tx_caldaydescription_cal_descriptionmarker.php
+++ hooks/class.tx_caldaydescription_cal_descriptionmarker.php
@@ -28,12 +28,15 @@ require_once (PATH_tslib.'class.tslib_pibase.php');
  * @subpackage caldaydescription
  */
 class tx_caldaydescription_cal_descriptionmarker {
-
+	var $xmlData = '';
+	 
  	function tx_caldaydescription_cal_descriptionmarker (){
- 		global $LANG;
- 		$LANG->includeLLFile('EXT:caldaydescription/static/locallang.xml');
- 		global $CONF;
- 		debug($CONF);
+ 		global $TSFE;
+ 		global $xmlData;
+ 		
+		$xmlPath = t3lib_div::getFileAbsFileName($TSFE->tmpl->setup['plugin.']['tx_caldaydescription.']['descriptionLL']);
+		$xmlFile = t3lib_div::getURL($xmlPath);
+		$xmlData = t3lib_div::xml2array($xmlFile);
 	}
 	
 	/**
@@ -42,19 +45,30 @@ class tx_caldaydescription_cal_descriptionmarker {
 	 */
 	function postSearchForObjectMarker($top, $template, &$sims, $rems, $wrapped, $view) {
 		global $LANG;
+ 		global $xmlData;
+		
 		if (($view=='day')&&(!is_null($top->conf['getdate']))) {
-			$sims['###CALDAYDESCRIPTION###']=$LANG->getLL('l_cdd_'.$top->conf['getdate']);
+			$sims['###CALDAYDESCRIPTION###']=$xmlData['data']['default']['l_cdd_'.$top->conf['getdate']];
 		} elseif (($view=='week')&&(!is_null($top->days))){
 			foreach ($top->days as $day) {
-				$sims['###CALDAYDESCRIPTION'.$day->weekdayNumber.'###'] = $LANG->getLL('l_cdd_'.$day->Ymd);
+				$sims['###CALDAYDESCRIPTION'.$day->weekdayNumber.'###'] = $xmlData['data']['default']['l_cdd_'.$day->Ymd];
 			}
 		} elseif (($view=='event')&&(!is_null($top->conf['getdate']))){
-			$sims['###CALDAYDESCRIPTION###']=$LANG->getLL('l_cdd_'.$top->conf['getdate']);
+			$sims['###CALDAYDESCRIPTION###']=$xmlData['data']['default']['l_cdd_'.$top->conf['getdate']];
 
 		} elseif (($view=='list')&&(!is_null($top->cachedValueArray['start_date']))){
-			$sims['###CALDAYDESCRIPTION###']=$LANG->getLL('l_cdd_'.$top->cachedValueArray['start_date']);
+			$sims['###CALDAYDESCRIPTION###']=$xmlData['data']['default']['l_cdd_'.$top->cachedValueArray['start_date']];
+		} elseif (($view=='locationgrid')&&(is_array($top->objectsInList))){
+			$dayItems = 0;
+			foreach (array_keys($top->objectsInList) as $datekey) {
+				$sims['###CALDAYDESCRIPTION'.intval($dayItems).'###'] = $xmlData['data']['default']['l_cdd_'.$datekey];
+				$dayItems++;
+				$sims['###CALDAYDESCRIPTION###'][] = $xmlData['data']['default']['l_cdd_'.$datekey];
+			}
 		}else {
-		//	debug($view);
+			// Found an unsupported view? Please enable the debug statements and send the resuslt with a description to the EXTs author.	
+			//debug($top->objectsInList);
+			//debug($view,'view');
 		}
 
 	}
